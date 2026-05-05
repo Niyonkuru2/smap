f8558cdeae2c74eada0d8dfd34a8bf0a204bd84d
niyokuru2 <niyonkurusam5@gmail.com>
2026-05-05 15:38:42 +0200

    cors

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -7,6 +7,7 @@ export const corsOptions = {
             'http://localhost:3000',
             'http://127.0.0.1:5173',
             'http://127.0.0.1:3000',
+            'https://smap-kappa.vercel.app'
         ];
         
         if (!origin || allowedOrigins.includes(origin)) {
